58cdfce636a05202ed8940fb39a05829bf7f555a
Kevin Wang <kayhne999@gmail.com>
2024-02-03 16:26:13 -0500

    gpt2_m_path algo update
    added new vocab + increased randomness

--- gpt2_m_path_tone_bot.py
+++ gpt2_m_path_tone_bot.py
@@ -26,7 +26,19 @@ angry_words = ["hate",
                "kys", 
                "hell", 
                "angry", 
-               "bruh"]
+               "bruh",
+               "SHUT UP",
+               "annoyed",
+               ">:(",
+               ">:T",
+               "😤",
+               "😠",
+               "😡",
+               "🤬",
+               "bruh",
+               "what.",
+               "NOT"
+               ]
 
 sad_words = ["bitter", 
              "dismal", 
@@ -52,7 +64,18 @@ sad_words = ["bitter",
              "weep", 
              "low", 
              "unhappy", 
-             "troubled"]
+             "troubled",
+             ":(",
+             ":\'(",
+             "😥",
+             "😓",
+             "☹",
+             "🙁",
+             "😭",
+             "😢",
+             "😦",
+             "crying"
+            ]
 
 scared_words = ["afraid", 
                 "anxious", 
@@ -63,9 +86,18 @@ scared_words = ["afraid",
                 "startle", 
                 "petrif", 
                 "shake", 
-                "terrif", 
+                "terrified", 
                 "aghast", 
-                "terror"]
+                "terror", 
+                "terrify", 
+                "terrifying", 
+                "terror",
+                "oh no",
+                "😱",
+                "😰",
+                "😨",
+                ":\'0"
+                ]
 
 disgust_words = ["suck", 
                  "appal", 
@@ -87,9 +119,31 @@ disgust_words = ["suck",
                  "gross", 
                  "squeam", 
                  "indignant", 
-                 "invasion", "freak"]
+                 "invasion", 
+                 "freak",
+                 "ugh",
+                 "🤢",
+                 "🤮"
+                ]
 
-negative_words = angry_words + sad_words + scared_words + disgust_words
+disappointed_words = ["man...",
+                      "alright...",
+                      "...",
+                      "ok...",
+                      ":/",
+                      ":T",
+                      "yeah...",
+                      "💀",
+                      "😶",
+                      "😐",
+                      "😑",
+                      "🙄",
+                      "okay.",
+                      "😔",
+                      "😞"
+                     ]
+
+negative_words = angry_words + sad_words + scared_words + disgust_words + disappointed_words
 
 # Happy words (green):
 
@@ -125,14 +179,67 @@ happy_words = ["cheerful",
                "live", 
                "laugh", 
                "pink", 
-               "amaz", 
+               "amazing",
+               "amazed",
+               "amaze", 
                "good", 
                "ha", 
                "har", 
                "win",
                "fine", 
                "better", 
-               "pass"]
+               "pass", 
+               "harhar", 
+               "harharhar", 
+               "harharharhar", 
+               "ha", 
+               "haha", 
+               "hahaha", 
+               "hahahahaha",
+               "yippee",
+               "hooray",
+               "nice",
+               ":)",
+               ":D",
+               "let's go!",
+               "omg",
+               "amazing!",
+               "☺",
+               "😊",
+               "🤩",
+               "😃",
+               "😁",
+               "😄",
+               "smile",
+               "smiling",
+               "smiled",
+               "smiles",
+               "grin",
+               "grinning", 
+               "grinned",
+               "grins",
+               "happy"]
+
+amused_words = ["hehe",
+                "LOL",
+               "XD",
+               "LMAO",
+               "LMFAO",
+               "haha",
+               "muahaha",
+                ":3",
+                ":>",
+                "real",
+                ":P",
+                "😋",
+                "😏",
+                "🤭",
+                "😛",
+                "😝",
+                "😜"
+                ]
+
+positive_words = happy_words + amused_words
 
 # Neutral words (gray):
 
@@ -149,7 +256,9 @@ surprised_words = ["shock",
                    "reveal", 
                    "bomb", 
                    "stun", 
-                   "amaz", 
+                   "amazing",
+                   "amazed",
+                   "amaze", 
                    "wonder", 
                    "thunder", 
                    "wrinkle", 
@@ -157,7 +266,18 @@ surprised_words = ["shock",
                    "hell", 
                    "fuck", 
                    "shit", 
-                   "bruh"]
+                   "bruh",
+                   "what?!",
+                   "😲",
+                   ":0",
+                   "🤯", 
+                   "😳", 
+                   "omg",
+                   "o", 
+                   "oh", 
+                   "oh my", 
+                   "oh my god"
+                   ]
 
 model_name = "gpt2"
 
@@ -165,38 +285,47 @@ gpt2_lm = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
 def detect_emotion(sample_message):
-    
-    # loop for some words, add a random compile word, then continue
+    """Given some sample user message(s), return the probability
+    that the user is feeling some emotion.
+
+    Args:
+        sample_message (String): A message/messages a user says.
+
+    Returns:
+        dict: A dictionary of emotions and the likelihood that the 
+        user is experiencing them.
+    """
+    input_ids = tokenizer.encode(sample_message, return_tensors="pt") # 1 x n size (n is # of tokens)
     
     expanded_sample = ""
     
     # 100 -> 6 minutes
     # 25 -> 47 seconds
-    input_ids = tokenizer.encode(sample_message, return_tensors="pt") # 1 x n size (n is # of tokens)
-
-    output1 = gpt2_lm.generate(
-        input_ids, 
-        max_length=input_ids.shape[1]+18, 
-        num_beams=5, 
-        no_repeat_ngram_size=2, 
-        top_k=50, 
-        top_p=0.95, 
-        temperature=0.7, 
-    ) # 1 x 18
-    expanded_sample += tokenizer.decode(output1[0], skip_special_tokens=True) # String
-
-    output2 = gpt2_lm.generate(
-        input_ids, 
-        max_length=input_ids.shape[1]+12, # size of max_length must always be greater than the input token size
-        num_beams=5, 
-        no_repeat_ngram_size=2, 
-        top_k=50, 
-        top_p=0.95, 
-        temperature=1.3, 
-    )
-    expanded_sample += tokenizer.decode(output2[0], skip_special_tokens=True)
-    # to prevent 'i am feel' looping
+    # Current implementation - 
+    for i in range(2):
+        output1 = gpt2_lm.generate(
+            input_ids, 
+            max_length=input_ids.shape[1]+18, 
+            num_beams=5, 
+            no_repeat_ngram_size=2, 
+            top_k=50, 
+            top_p=0.95, 
+            temperature=0.7, 
+        ) # 1 x 18
+        expanded_sample += tokenizer.decode(output1[0], skip_special_tokens=True) # String
     
+        output2 = gpt2_lm.generate(
+            input_ids, 
+            max_length=input_ids.shape[1]+12, # size of max_length must always be greater than the input token size
+            num_beams=5, 
+            no_repeat_ngram_size=2, 
+            top_k=50, 
+            top_p=0.95, 
+            temperature=1.3, 
+        )
+        expanded_sample += tokenizer.decode(output2[0], skip_special_tokens=True)
+        # to prevent 'i am feel' looping
+        
     print("---------------------------------------------------------------")
     print(f"Expanded message: {expanded_sample}") # Debug print, comment out in beta
     print("---------------------------------------------------------------")
@@ -205,11 +334,14 @@ def detect_emotion(sample_message):
     n = len(text_split)
     emotions_likelihood = {
         "negative": 0,
+        "positive": 0,
         "angry": 0,
         "sad": 0,
         "scared": 0,
         "disgust": 0,
+        "disappointed": 0,
         "happy": 0,
+        "amused": 0,
         "surprised": 0
     }
     for i in text_split:
@@ -219,16 +351,25 @@ def detect_emotion(sample_message):
                 emotions_likelihood["angry"] += 1
             elif i in sad_words:
                 emotions_likelihood["sad"] += 1
+            elif i in disgust_words:
+                emotions_likelihood["disgust"] += 1
+            elif i in disappointed_words:
+                emotions_likelihood["disappointed"] += 1
             else:
                 emotions_likelihood["scared"] += 1
-        elif i in happy_words:
-            emotions_likelihood["happy"] += 1
+        elif i in positive_words:
+            emotions_likelihood["positive"] += 1
+            if i in happy_words:
+                emotions_likelihood["happy"] += 1
+            else:
+                emotions_likelihood["amused"] += 1
         elif i in surprised_words:
             emotions_likelihood["surprised"] += 1
     for key in emotions_likelihood.keys():
         emotions_likelihood[key] = str(emotions_likelihood[key] / n * 100) + "%" 
     return emotions_likelihood
 
+   
 if __name__ == "__main__":
     sample_messages = [
         "Freaking hell mfs acting like THEY are the groundhogs", 
